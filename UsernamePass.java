bbb972b82468769fed4b257f97fbfb4b9322eac7
arunkumarkumar <115149778+arunkumarkumar@users.noreply.github.com>
2022-11-01 04:24:40 -0700

    Update UsernamePass.java

--- UsernamePass.java
+++ UsernamePass.java
@@ -1,8 +1,20 @@
-package com.dt;
+package com.java.Onlineshopping;
 
 public class UsernamePass {
 	private String userpass;
 	private String password;
+	private String groceryproduct;
+	private String mobileproduct;
+	private String menproduct;
+	private String womenprdoct;
+	private String electroniprodu;
+	private String applianproduct;
+	public String getElectroniprodu() {
+		return electroniprodu;
+	}
+	public void setElectroniprodu(String electroniprodu) {
+		this.electroniprodu = electroniprodu;
+	}
 	public String getUserpass() {
 		return userpass;
 	}
@@ -15,10 +27,38 @@ public class UsernamePass {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getGroceryproduct() {
+		return groceryproduct;
+	}
+	public void setGroceryproduct(String groceryproduct) {
+		this.groceryproduct = groceryproduct;
+	}
+	public String getMobileproduct() {
+		return mobileproduct;
+	}
+	public void setMobileproduct(String mobileproduct) {
+		this.mobileproduct = mobileproduct;
+	}
+	public String getMenproduct() {
+		return menproduct;
+	}
+	public void setMenproduct(String menproduct) {
+		this.menproduct = menproduct;
+	}
+	public String getWomenprdoct() {
+		return womenprdoct;
+	}
+	public void setWomenprdoct(String womenprdoct) {
+		this.womenprdoct = womenprdoct;
+	}
+	public String getApplianproduct() {
+		return applianproduct;
+	}
+	public void setApplianproduct(String applianproduct) {
+		this.applianproduct = applianproduct;
+	}
 	
 
  
 	
 }
-
-
